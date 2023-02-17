7dce203e1879cd811a805bc3e6e6af1d1e3fb445
Chien-Ho <67454100+Chien-Ho@users.noreply.github.com>
2023-02-17 14:09:33 +0700

    first commit

---
+++ target/run/antoree/models/example/my_second_dbt_model.sql
@@ -0,0 +1,8 @@
+
+  create view `antoree_warehouse_2`.`my_second_dbt_model__dbt_tmp` as (
+    -- Use the `ref` function to select from other models
+
+select *
+from `antoree_warehouse_2`.`my_first_dbt_model`
+where id = 1
+  );
